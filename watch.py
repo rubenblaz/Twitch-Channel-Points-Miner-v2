42e8f285844d2c522e226913d0857cfe9fad7bff
rubenblaz <rubenblazrguez@gmail.com>
2023-03-07 12:31:02 +0100

    Update watch.py

--- watch.py
+++ watch.py
@@ -20,11 +20,5 @@ def hello_world():
 
 if __name__ == '__main__':
     #port = int(os.environ.get('PORT', 8000))
-    #app.run()
-    channels = os.environ.get("channels")
-    twitch_miner = TwitchChannelPointsMiner(
-        username=os.environ.get("username"),
-        password=os.environ.get("password"),            # If no password will be provided, the script will ask interactively
-        claim_drops_startup=True,                  # If you want to auto claim all drops from Twitch inventory on the startup
-    )
-    twitch_miner.mine(channels.split(','), followers=False) 
+    app.run()
+    request.get('/')
